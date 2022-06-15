69f3ed14758a9b554be39212dc75930be1696882
Veda Bezaleel <vedabezaleel@gmail.com>
2022-06-16 00:22:30 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    /* 'localhost',
+    pada kasus cloud, localhost bisa diubah menjadi 0.0.0.0 bila diterapkan pada google cloud,
+    karena mengguankan internal ip repot. 0.0.0.0
+    berarti ip dapet diakses dari ip mana saja seperti '*' untuk <any>
+    */
     routes: {
       cors: {
         origin: ['*'], // untuk kasus same-origin buka pengaturan browser di chrome://flags/
